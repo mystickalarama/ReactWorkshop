b8e545e49010bf71ca185b3fd1d198770e84fc29
Sinan Kerem Doğan <kerem_force@hotmail.com>
2024-10-05 14:23:21 +0300

    The product card dimensions have been fixed.

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -6,9 +6,9 @@ const ProductCart = ({ product, onAddToCart }) => {
     <Card className="product-card">
       <img src={product.image} alt={product.title} className="product-image" />
       <CardBody className="d-flex flex-column">
-        <CardTitle tag="h5">{product.title}</CardTitle>
+        <CardTitle tag="h5" className="product-title">{product.title}</CardTitle>
         <div className="mt-auto">
-          <h6 className="text-primary">${product.price.toFixed(2)}</h6>
+          <h6 className="product-price">${product.price.toFixed(2)}</h6>
           <Button color="success" onClick={() => onAddToCart(product)}>Sepete Ekle</Button>
         </div>
       </CardBody>
